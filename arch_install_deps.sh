16da5dc3b7d723c97866a2b80b37ee4a0985d0ad
Dog Tag <dogtag@protonmail.com>
2018-01-21 19:18:07 +0000

    changes in build scripts

--- arch_install_deps.sh
+++ arch_install_deps.sh
@@ -3,16 +3,18 @@
 NORMAL_USER=alarm
 
 if [ $# = 0 ]; then
-    VIRTUAL_ENV_DIR=Project
+    VIRTUAL_ENV_DIR=`pwd`
 else
     VIRTUAL_ENV_DIR=$1
 fi
 
-echo -e "Virtual Environment will be created as" ${VIRTUAL_ENV_DIR}
+echo "===================== Deployment Initiated ==================="
+echo -e "Virtual Environment will be created in..." ${VIRTUAL_ENV_DIR}
 
 echo -ne "Checking the current user permissions... "
 if ! [ $(id -u) = 0 ]; then
    echo "not root!"
+   echo
    echo "CRITICAL: This script must be run as root"
    exit 1
 else
@@ -20,35 +22,40 @@ else
 fi
 
 echo -ne "updating the kernel with pacman... "
-#pacman -Suy --noconfirm >| /dev/null
+pacman -Suy --noconfirm >| /dev/null
 echo -e "completed"
 
 echo -ne "Installing the dependencies... "
-#pacman -S --noconfirm vim git unzip make gcc python2-virtualenv >| /dev/null
+pacman -S --noconfirm vim git unzip make gcc python2-virtualenv >| /dev/null
 echo -e "completed"
 
+echo -e "-------------------- Performing Dependency checks ---------------------"
+echo
 echo -e "Dependency check after the installation... initiated"
 for pkg in {git,unzip,make,gcc,python2-virtualenv}; do
     pacman -Q $pkg >| /dev/null;
     if ! [ $? = 0 ]; then
-        echo -e "--> failed for the package" $pkg;
+        echo -e "--> Package" $pkg "not installed :-(";
         exit 1;
     else
-        echo -e "--> succeeded for the package" $pkg;
+	echo -e "--> Package" $pkg "installed and verified successfully :-).";
     fi;
 done
 echo -e "Dependency check after the installation... completed"
 
 echo
-echo -ne "setting up the virtualenv as " ${VIRTUAL_ENV_DIR} "... "
+echo -e "setting up the virtualenv in " ${VIRTUAL_ENV_DIR} "... "
+echo
 su ${NORMAL_USER} -c "virtualenv2 ${VIRTUAL_ENV_DIR}"
-echo -e "completed"
-
-if ! [ -d ${VIRTUAL_ENV_DIR} ]; then
-    echo -e "Virtualenv" ${VIRTUAL_ENV_DIR} "is not created"
-    exit 1
-fi
 
 echo
-su ${NORMAL_USER} -c "cd ${VIRTUAL_ENV_DIR}; ./bin/pip install -r requirements.txt"
+echo -e "-------------------- Installing pip requirements ---------------------"
+echo
+su ${NORMAL_USER} -c "./bin/pip install -r requirements.txt"
+echo
+echo -e "-------------------- Installing Buildout ---------------------"
+echo
+su ${NORMAL_USER} -c "./bin/buildout -c buildout.cfg"
+echo
+echo "===================== Deployment Completed Successfully ==================="
 
